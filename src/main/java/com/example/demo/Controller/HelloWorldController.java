611185d7df64374266919f6969cf7915ed0ef984
Administrator <admin@example.com>
2018-12-13 08:56:29 +0000

    Update HelloWorldController.java

--- src/main/java/com/example/demo/Controller/HelloWorldController.java
+++ src/main/java/com/example/demo/Controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
