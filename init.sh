101e21399d817490576ee7863abd2ae48913f7f3
root <956043615@qq.com>
2018-12-25 02:28:50 +0000

    add env etcd

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+system_conf_dir=/usr/lib/systemd/system/
+env_dir=/etc/sysconfig/
 ##initialization config need for kubernetes cluster node
 #off virtual memory
 swapoff -a
@@ -6,5 +8,7 @@ swapoff -a
 sed -i /swap/d /etc/fstab 
 #enabled master to node tranfale ssh tun
 sshd-keygen
-#
-
+#copy etcd conf and binary prog
+cp bin/etcd* /usr/local/bin
+cp etcd.service ${system_conf_dir}
+cp etcd ${env_dir}
