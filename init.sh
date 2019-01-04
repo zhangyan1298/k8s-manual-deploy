b8785b999fd524757a116a0cae65b34952a73a71
root <956043615@qq.com>
2019-01-04 02:35:26 +0000

    add calico docker config

--- init.sh
+++ init.sh
@@ -76,4 +76,21 @@ cp bin/cert/*  /etc/kubernetes/ssl/
 #--hostname-override=10.0.0.6
 #如果这里的hostname设置了相同的值，那么master上只能显示出一个node，解决
 #需要删除master签发的/etc/kubernetes/kubelet.kubeconfig 文件，修改hostname 重新注册
+###配置kube-proxy
+##kube-proxy直接使用kubelet 的tls文件进行认证
+###拷贝二进制文件与配置文件
+##修改hostname
+##更改proxymode，默认未iptables，如果iptables 错误那么将回退到userspace，这里使用
+#ipvs
+#--proxy-mode=ipvs
+##--ipvs-scheduler=rr ,ipvs负载平衡算法
+##支持的有 lc,dh,sh,sed,nq
 #配置集群网络
+#使用calico 三层路由模式，并开启tun隧道
+#官网下载calico.yml ，修改endpoints etcd地址，如果etcd开启了tls 那么加入tls，ca，key，cert 参数，修改volume 挂载configmap。
+#calico文件包括一个kube-caloico controller deployment 部署，一个calico-node pod daemonset，运行在集群内每台主机上，使用hostnetwor-true 模式
+#calcio包括felix，用来监控容器，设置容器ip,路由规则,iptables 等.bird 分发bgp peer 对等路由信息. confd 监控etcd 生成bird配置文件.
+#参数注意实现ipipMode=always，开启ipip隧道模式，如果未开启则使用bgp模式下node 无法ping通pod 的ip，在公有云环境下开启ipipmode=always
+#修改kubelet 的network-plugin=cni,可选参数--cni-conf-dir 插件的配置文件所在地--cni-bin-dir cni插件的bin文件目录
+#docker 增加daemon.json 文件，开启独立群主机选项
+#
